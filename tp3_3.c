9dc8c3e34c77ca4b910155cb9927b6850b162f54
Sofia Rodriguez del Busto <rdbsofi@gmail.com>
2024-04-16 13:15:14 -0300

    Agrego punto 3 de cadena de caracteres

--- tp3_3.c
+++ tp3_3.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main()
 {
+    char **nombres =(char **)malloc(5*sizeof(char *));
+    char *buff = (char *)malloc(sizeof(char)*100);
+    for(int i = 0;i<5;i++)
+    {
+        printf("Ingrese el nombre %d", i+1);
+        gets(buff);
+        nombres[i] =(char *) malloc((strlen(buff)+1)*sizeof(char));
+        strcpy(nombres[i], buff);
+    }
+    for(int j=0;j<5;j++)
+    { 
+        puts(nombres[j]);
+    }
+    free(buff);
+    for (int k = 0; k < 5; k++)
+    {
+       free(nombres[k]);
+    }
+    free(nombres);
     
 }
